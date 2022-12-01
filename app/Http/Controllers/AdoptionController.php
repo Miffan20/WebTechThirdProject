4baf121ce89edcc9e2c0426b8b539da3da01d6aa
Pizzafuglen <Crose19@student.sdu.dk>
2022-12-01 11:03:57 +0100

    User task 3 partial done

--- app/Http/Controllers/AdoptionController.php
+++ app/Http/Controllers/AdoptionController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Adoption;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 
 class AdoptionController extends Controller
@@ -23,7 +25,9 @@ class AdoptionController extends Controller
         | This is done using the adopted_by field from the user column in the database.
         |-----------------------------------------------------------------------
         */
-
+        if (Auth::user()) {
+            $adoption ->update(['adopted_by' => Auth::id()]);
+        }
 
         return redirect('/')->with('success', "Pet $adoption->name adopted successfully");
     }
